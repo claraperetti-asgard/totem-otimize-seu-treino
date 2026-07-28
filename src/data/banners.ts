3b924158bd8584a874434c1adae9f7e19308ee78
claraperetti-asgard <clara.peretti@asgardtech.com.br>
2026-07-28 15:05:48 -0300

    fix: add new banners

--- src/data/banners.ts
+++ src/data/banners.ts
@@ -7,6 +7,8 @@
 import banner1 from '../assets/banners/banner1.jpeg'
 import banner2 from '../assets/banners/banner2.jpeg'
 import banner3 from '../assets/banners/banner3.jpeg'
+import banner4 from '../assets/banners/banner4.jpeg'
+import banner5 from '../assets/banners/banner5.jpeg'
 
 export interface Banner {
   id: string
@@ -18,5 +20,7 @@ export interface Banner {
 export const banners: Banner[] = [
   { id: 'banner1', imagem: banner1, alt: 'Aviso da academia 1' },
   { id: 'banner2', imagem: banner2, alt: 'Aviso da academia 2' },
-  { id: 'banner3', imagem: banner3, alt: 'Aviso da academia 3' },
+  { id: 'banner4', imagem: banner4, alt: 'Aviso da academia 4' },
+  { id: 'banner5', imagem: banner5, alt: 'Aviso da academia 5' },
+  { id: 'banner3', imagem: banner3, alt: 'Aviso da academia 3' }
 ]
